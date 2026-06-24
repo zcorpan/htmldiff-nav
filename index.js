800611f49c88cf3abbef95b340d6c40942cdcac4
Simon Pieters <zcorpan@gmail.com>
2026-06-24 11:19:08 +0200

    Add checkboxes to hide old and markers; tweak current styling
    Also use `button` instead of `div` for buttons.
    
    This depends on https://github.com/w3c/htmldiff-ui/pull/26

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
         changes = Array.prototype.slice.call(changes, 0);
         var index = -1;
         var length = changes.length;
-        
+
         return {
             index: function() {
                 return index + 1;
@@ -16,17 +16,23 @@
                 return changes[index];
             },
             previous: function() {
-                index--;
-                if (index < 0) {
-                    index = length-1;
-                }
+                do {
+                    index--;
+                    if (index < 0) {
+                        index = length-1;
+                        break;
+                    }
+                } while (!isRendered(changes[index]));
                 return changes[index];
             },
             next: function() {
-                index++;
-                if (index >= length) {
-                    index = 0;
-                }
+                do {
+                    index++;
+                    if (index >= length) {
+                        index = 0;
+                        break;
+                    }
+                } while (!isRendered(changes[index]));
                 return changes[index];
             },
             none: function() {
@@ -34,32 +40,78 @@
             }
         }
     }
-    
+
+    function createCheckbox(label, fn, key, defaultChecked) {
+        var rv = document.createElement("label");
+        var input = document.createElement("input");
+        input.type = "checkbox";
+        input.onchange = function(event) {
+          fn(input);
+          localStorage[key] = String(input.checked);
+        }
+        input.defaultChecked = getStorage(key, defaultChecked);
+        rv.appendChild(input);
+        rv.appendChild(document.createTextNode(" " + label));
+        if (input.defaultChecked !== defaultChecked) {
+          fn(input);
+        }
+        return rv;
+    }
+
+    function toggleHideOld(checkbox) {
+      document.documentElement.classList.toggle('hide-diff-old', !(checkbox.checked));
+    }
+
+    function toggleHideMarkers(checkbox) {
+      document.documentElement.classList.toggle('hide-diff-markers', !(checkbox.checked));
+    }
+
+    function getStorage(key, defaultValue) {
+      if (key in localStorage) {
+        return localStorage[key] === "true";
+      }
+      return defaultValue;
+    }
+
+    function isRendered(el) {
+        return el.offsetWidth || el.offsetHeight || el.getClientRects().length;
+    }
+
     function init() {
-        var CSS_TEXT = "float: left; padding: 5px 15px; border-left: 1px solid #999; cursor: pointer;"
-        var CSS_BORDER = "outline: 2px dashed #00F";
+        var BUTTON_CSS_TEXT = "appearance: none; float: left; padding: 5px 15px; border: none; border-left: 1px solid #999; font: inherit; background-color: transparent; color: inherit;";
+        var LABEL_CSS_TEXT = "float: left; padding: 1px 5px; border: none; border-left: 1px solid #999;";
+        var CHECKBOX_CSS_TEXT = "vertical-align: middle;";
+        var CSS_BORDER = "border-bottom: 2px solid #00F;";
         var container = document.createElement("div");
         var display = document.createElement("div");
-        var previous = document.createElement("div");
-        var next = document.createElement("div");
+        var old = createCheckbox("old", toggleHideOld, "htmldiffOldChecked", true);
+        var markers = createCheckbox("markers", toggleHideMarkers, "htmldiffMarkersChecked", true);
+        var previous = document.createElement("button");
+        var next = document.createElement("button");
         var position = document.createElement("span");
         var warning = document.querySelector(".annoying-warning");
         display.appendChild(position);
         container.appendChild(display);
+        container.appendChild(old);
+        container.appendChild(markers);
         container.appendChild(previous);
         container.appendChild(next);
         document.body.appendChild(container);
         container.className = "w3c-htmldiff-nav";
         container.style.cssText = "background-image: linear-gradient(transparent,rgba(0,0,0,.05) 40%,rgba(0,0,0,.1)); font: 12px sans-serif; color: #666; border-top: 1px solid #999; border-right: 1px solid #999; border-left: 1px solid #999; position: fixed; bottom: 0; right: 25px; border-radius: 3px 3px 0 0; background-color: #eee; z-index: 2147483647;"
-        next.style.cssText = CSS_TEXT;
-        previous.style.cssText = CSS_TEXT;
+        old.style.cssText = LABEL_CSS_TEXT;
+        old.firstChild.style.cssText = CHECKBOX_CSS_TEXT;
+        markers.style.cssText = LABEL_CSS_TEXT;
+        markers.firstChild.style.cssText = CHECKBOX_CSS_TEXT;
+        next.style.cssText = BUTTON_CSS_TEXT;
+        previous.style.cssText = BUTTON_CSS_TEXT;
         display.style.cssText = "float: left; padding: 5px 5px;"
         position.style.cssText = "background-color: #fff; border: 1px solid #999; padding: 1px 5px; box-shadow: inset 0 1px 3px #ddd; border-radius: 2px"
         next.textContent = "next ›";
         next.title = "Keyboard nav: use \"j\" to jump to next change."
         previous.textContent = "‹ previous";
         previous.title = "Keyboard nav: use \"k\" to jump to previous change.";
-        
+
         function update(state) {
             position.textContent = state.index() + " of " + state.size();
         }
@@ -86,7 +138,7 @@
             scrollIntoView(true);
             update(state);
         }
-        
+
         function onprevious(e) {
             if (warning && warning.open) {
                 warning.open = false;
@@ -108,11 +160,9 @@
             current = null;
             update(state);
         }
+
         var selector = document.querySelector("script[data-navigable-selector]")? document.querySelector("script[data-navigable-selector]").dataset["navigableSelector"] : "del.diff-old, ins.diff-chg, ins.diff-new";
         var diffs = document.querySelectorAll(selector);
-        diffs = Array.prototype.filter.call(diffs, function(e) {
-            return e.offsetWidth || e.offsetHeight || e.getClientRects().length;
-        });
         var current = null;
         var state = new State(diffs);
         next.onclick = onnext;
@@ -132,7 +182,6 @@
         if(document.readyState === "complete") {
             clearInterval(interval);
             init();
-        }    
+        }
     }, 100);
 })(window);
-
