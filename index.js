04658d04e1129b637aa3f2eeca71c936b65cd65b
Simon Pieters <zcorpan@gmail.com>
2026-06-24 11:44:00 +0200

    Improve dark mode

--- index.js
+++ index.js
@@ -89,7 +89,7 @@
         var BUTTON_CSS_TEXT = "appearance: none; float: left; padding: 5px 15px; border: none; border-left: 1px solid #999; font: inherit; background-color: transparent; color: inherit;";
         var LABEL_CSS_TEXT = "float: left; padding: 1px 5px; border: none; border-left: 1px solid #999;";
         var CHECKBOX_CSS_TEXT = "vertical-align: middle;";
-        var CSS_BORDER = "border-bottom: 2px solid #00F;";
+        var CSS_BORDER = "border-bottom: 2px solid #00F; border-bottom-color: light-dark(#00F, #DDD)";
         var container = document.createElement("div");
         var display = document.createElement("div");
         var old = createCheckbox("old", toggleHideOld, "htmldiffOldChecked", true);
